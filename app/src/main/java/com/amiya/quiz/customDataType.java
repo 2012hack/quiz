324b7797a70e07e51ddc71c3c6cbacdf5ffcc71c
2012hack <165986380+2012hack@users.noreply.github.com>
2024-04-23 20:21:56 +0530

    added

---
+++ app/src/main/java/com/amiya/quiz/customDataType.java
@@ -0,0 +1,12 @@
+package com.amiya.quiz;
+
+public class customDataType {
+    public String  quizQuestion;
+    public String quizAnswer;
+
+    customDataType(String quizQuestion, String quizAnswer){
+        quizQuestion = quizQuestion;
+        quizAnswer = quizAnswer;
+
+    }
+}
